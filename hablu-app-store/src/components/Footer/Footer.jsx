bd6572fb87b40f6c956bcab74613117d8e82da4d
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-10-09 07:36:55 +0600

    Update Footer.jsx

--- hablu-app-store/src/components/Footer/Footer.jsx
+++ hablu-app-store/src/components/Footer/Footer.jsx
@@ -3,28 +3,12 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="flex-col w-full text-center bg-base-200 text-base-content rounded p-10 space-y-10 shadow-[0_0_5px_rgba(9,9,9,0.2)]">
-      <div className="flex-col md:flex-row justify-around items-center">
+      <div className="flex-col md:flex justify-around items-center">
         <div className="grid grid-flow-col gap-4 [&>a]:underline-offset-[5px] hover:[&>a]:decoration-2">
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Dev Forum</a>
           <a className="link link-hover">Publish App</a>
-        </div>
-
-        <div>
-          <div className="flex-col space-y-3 text-center">
-            <h6 className="footer-title">Subscribe to our Newsletter!</h6>
-            <fieldset className="w-80">
-              <div className="join">
-                <input
-                  type="email"
-                  placeholder="Your email here"
-                  className="input input-bordered join-item focus:outline-none "
-                />
-                <button className="btn hablu-gradient join-item text-white">Subscribe</button>
-              </div>
-            </fieldset>
-          </div>
         </div>
       </div>
 
