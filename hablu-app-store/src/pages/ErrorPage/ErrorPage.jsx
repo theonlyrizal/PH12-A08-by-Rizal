a32d902f5fa7d56573e6dba48ac4447673aee14d
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-10-10 11:24:24 +0600

    Update ErrorPage.jsx

--- hablu-app-store/src/pages/ErrorPage/ErrorPage.jsx
+++ hablu-app-store/src/pages/ErrorPage/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
   return (
     <div className="w-full h-[100vh] flex flex-col justify-center items-center space-y-4">
       <img src={error404} alt="Page not found" className="max-w-xs" />
-      <h2 className="text-2xl font-semibold">Maybe we followed the wrong Track!</h2>
+      <h2 className="text-2xl text-center font-semibold">Maybe we followed the wrong Track!</h2>
       <p className="text-gray-500">But Life will find its way back</p>
       <div className="flex space-x-2">
         <Link to={-1} className="btn hablu-gradient text-white">
